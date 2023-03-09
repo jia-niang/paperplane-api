3ab65a60ce02a7842d0430a56977c6e8db4da53a
jia-niang <1@paperplane.cc>
2023-03-09 20:27:51 +0800

    refactor(cos): 现在 COS 存储桶的配置使用配置文件

--- src/utils/cos.ts
+++ src/utils/cos.ts
@@ -1,8 +1,8 @@
 import COS from 'cos-nodejs-sdk-v5'
 
 export const cos = new COS({
-  SecretId: 'AKIDpW4QdkWK34335yhuvmU2RhmuqcDblmaC',
-  SecretKey: 'bv3xfBe0pnjYItcDdsu7IJXfdZJwg0IZ',
+  SecretId: process.env.COS_SECRET_ID,
+  SecretKey: process.env.COS_SECRET_KEY,
 })
 
 export interface ICosFileUploadInfo {
