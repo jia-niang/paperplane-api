b4173aa82317c1e9b8732760e9fd67a70e4b1ce0
jia-niang <1@paperplane.cc>
2023-03-28 13:31:11 +0800

    feat(schemas/git): 新增 git 相关 schema

--- src/schemas/git.schema.ts
+++ src/schemas/git.schema.ts
@@ -1,12 +1,45 @@
-import { InjectModel, MongooseModule, Schema, SchemaFactory } from '@nestjs/mongoose'
+import { InjectModel, MongooseModule, Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
 import { Document } from 'mongoose'
 
 @Schema()
-export class Git extends Document {}
+export class GitRepo {
+  @Prop({ required: true })
+  name: string
+
+  @Prop({ required: true })
+  url: string
+}
+
+@Schema()
+export class GitStaff {
+  @Prop({ required: true })
+  name: string
+
+  @Prop({ required: true, default: [] })
+  emails: string[]
+
+  @Prop({ required: true, default: [] })
+  alternativeNames: string[]
+}
+
+@Schema()
+export class GitProject extends Document {
+  @Prop({ required: true })
+  name: string
+
+  @Prop({ required: true })
+  scope: string
+
+  @Prop({ required: true, type: [GitRepo], default: [] })
+  repos: GitRepo[]
+
+  @Prop({ required: true, type: [GitStaff], default: [] })
+  staff: GitStaff[]
+}
 
 const gitModelName = 'git'
 
 export const GitDBInject = () => InjectModel(gitModelName)
 export const GitModule = MongooseModule.forFeature([
-  { name: gitModelName, schema: SchemaFactory.createForClass(Git) },
+  { name: 'git', schema: SchemaFactory.createForClass(GitProject) },
 ])
