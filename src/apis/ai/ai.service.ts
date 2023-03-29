b6e48df00e91d0efb3be7af76796a5f098f1bcb3
jia-niang <1@paperplane.cc>
2023-03-30 02:17:57 +0800

    feat(apis/ai): 周报 API 现在更精简和准确

--- src/apis/ai/ai.service.ts
+++ src/apis/ai/ai.service.ts
@@ -39,10 +39,8 @@ export class AiService {
   async weekly(text: string, mode: weeklyModeType) {
     let ask = ''
 
-    if (mode === 'ymtd-fe') {
-      ask = `“医脉同道” 是一个医疗行业的招聘网站，我是一个 “医脉同道” 的前端工程师，请帮我把以下的工作内容填充为一篇完整的周报，分为5个章节：目标和进度、详细进展、复盘与思考、需要的帮助和支持、后续行动项，用 markdown 输出：${text}`
-    } else if (mode === 'career') {
-      ask = `请帮我把以下的工作内容填充为一篇完整的周报，分为5个章节：目标和进度、详细进展、复盘与思考、需要的帮助和支持、后续行动项，其中“目标与进度”章节内容固定为“待补充”，“复盘与思考”、“需要的帮助和支持”章节的内容固定为“暂无”，用 markdown 输出：${text}`
+    if (mode === 'career') {
+      ask = `一份周报分为5个章节：目标和进度、详细进展、复盘与思考、需要的帮助和支持、后续行动项，其中“目标与进度”章节内容固定为“待补充”，“复盘与思考”、“需要的帮助和支持”章节的内容固定为“暂无”，不需要大标题，请帮我把以下的工作内容填充为一篇完整的周报，用 markdown 格式以分点叙述的形式输出：${text}`
     } else {
       ask = `请帮我把以下的工作内容填充为一篇完整的周报，用 markdown 格式以分点叙述的形式输出：${text}`
     }
@@ -55,5 +53,6 @@ export class AiService {
       })
       .then(res => res.data)
       .then(res => res.choices?.[0].text)
+      .then(text => text.slice(text.indexOf('#')))
   }
 }
--- src/types/ai-biz.d.ts
+++ src/types/ai-biz.d.ts
@@ -2,6 +2,5 @@
  * 输出周报的格式
  * `"normal"` = 通常
  * `"career"` = 使用科锐国际
- * `"ymtd-fe"` = 使用医脉同道前端格式
  */
-type weeklyModeType = 'normal' | 'career' | 'ymtd-fe'
+type weeklyModeType = 'normal' | 'career'
