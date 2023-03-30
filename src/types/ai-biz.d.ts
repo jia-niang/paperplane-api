637510a9463a727a3c657fc72b982f91b1edded2
jia-niang <1@paperplane.cc>
2023-03-30 10:47:49 +0800

    feat(tools/weekly): 现在周报功能支持科锐前端

--- src/types/ai-biz.d.ts
+++ src/types/ai-biz.d.ts
@@ -1,6 +1,7 @@
 /**
  * 输出周报的格式
  * `"normal"` = 通常
- * `"career"` = 使用科锐国际
+ * `"career"` = 科锐国际
+ * `"career-fe"` = 科锐国际+前端
  */
-type weeklyModeType = 'normal' | 'career'
+type weeklyModeType = 'normal' | 'career' | 'career-fe'
