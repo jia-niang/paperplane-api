6de70e043949d922f6b525ff6ee342b0fa97732f
jia-niang <1@paperplane.cc>
2023-05-11 17:00:28 +0800

    feat: 新增全局日志配置

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { AppModule } from './app.module'
 import { HttpExceptionFilter } from './application/http-exception.filter'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn'],
+  })
   app.useGlobalFilters(new HttpExceptionFilter())
   await app.listen(6100)
 }
