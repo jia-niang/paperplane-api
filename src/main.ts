e039023139d340d20fb4f114f93339ffe8cfec23
jia-niang <1@paperplane.cc>
2023-05-11 17:28:07 +0800

    feat(app): 恢复全局日志配置

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,7 @@ import { AppModule } from './app.module'
 import { HttpExceptionFilter } from './application/http-exception.filter'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    logger: ['error', 'warn'],
-  })
+  const app = await NestFactory.create(AppModule)
   app.useGlobalFilters(new HttpExceptionFilter())
   await app.listen(6100)
 }
