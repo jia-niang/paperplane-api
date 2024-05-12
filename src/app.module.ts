aee1405ab11e5de48e9b40bbab32d9ce5641c2e5
jia-niang <1@paperplane.cc>
2024-05-13 03:54:57 +0800

    feat(app): 现在支持登录和身份认证

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
 import { RedisModule } from '@nestjs-modules/ioredis'
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
+import { APP_GUARD } from '@nestjs/core'
+import { JwtModule } from '@nestjs/jwt'
+import { PassportModule } from '@nestjs/passport'
 import { ServeStaticModule } from '@nestjs/serve-static'
 import { PrismaModule } from 'nestjs-prisma'
 import { createSoftDeleteMiddleware } from 'prisma-soft-delete-middleware'
@@ -8,6 +11,9 @@ import { createSoftDeleteMiddleware } from 'prisma-soft-delete-middleware'
 import { ResponseInterceptorProvider } from './app/response.interceptor'
 import { AiController } from './services/ai/ai.controller'
 import { AiService } from './services/ai/ai.service'
+import { AuthService } from './services/auth/auth.service'
+import { JwtAuthGuard } from './services/auth/jwt-auth-guard.service'
+import { JwtStrategy } from './services/auth/jwt.strategy'
 import { BusinessController } from './services/business/business.controller'
 import { BusinessService } from './services/business/business.service'
 import { DailyOffworkRecordService } from './services/daily-offwork/daily-offwork-record.service'
@@ -20,6 +26,8 @@ import { GitHelperService } from './services/git-helper/git-helper.service'
 import { MessageRobotController } from './services/message-robot/message-robot.controller'
 import { MessageRobotService } from './services/message-robot/message-robot.service'
 import { ThirdPartyService } from './services/third-party/third-party.service'
+import { UserController } from './services/user/user.controller'
+import { UserService } from './services/user/user.service'
 
 @Module({
   imports: [
@@ -51,6 +59,11 @@ import { ThirdPartyService } from './services/third-party/third-party.service'
       },
     }),
     ServeStaticModule.forRoot({ rootPath: __dirname + '/res', serveRoot: '/res' }),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '60d' },
+    }),
   ],
   controllers: [
     DockerStatusController,
@@ -59,6 +72,7 @@ import { ThirdPartyService } from './services/third-party/third-party.service'
     MessageRobotController,
     DailyOffworkController,
     BusinessController,
+    UserController,
   ],
   providers: [
     ResponseInterceptorProvider,
@@ -70,6 +84,10 @@ import { ThirdPartyService } from './services/third-party/third-party.service'
     DailyOffworkService,
     DailyOffworkRecordService,
     BusinessService,
+    UserService,
+    AuthService,
+    JwtStrategy,
+    { provide: APP_GUARD, useClass: JwtAuthGuard },
   ],
 })
 export class AppModule {}
--- src/services/ai/ai.controller.ts
+++ src/services/ai/ai.controller.ts
@@ -1,5 +1,7 @@
 import { Body, Controller, Get, Post } from '@nestjs/common'
 
+import { Public } from '@/app/auth'
+
 import { AiService } from './ai.service'
 
 interface IAiCompletionsBody {
@@ -11,21 +13,25 @@ interface IAiCompletionsBody {
 export class AiController {
   constructor(private readonly aiService: AiService) {}
 
+  @Public()
   @Get('/list')
   async listAllModels() {
     return this.aiService.listAllModels()
   }
 
+  @Public()
   @Post('/completions')
   async completions(@Body() body: IAiCompletionsBody) {
     return this.aiService.completions(body.text).then(res => ({ answer: res?.trimStart() }))
   }
 
+  @Public()
   @Post('/chat')
   async chat(@Body() body: IAiCompletionsBody) {
     return this.aiService.chat(body.text).then(res => ({ answer: res?.trimStart() }))
   }
 
+  @Public()
   @Post('/multiple-chat')
   async multipleChat(@Body() body: IAiCompletionsBody) {
     if (typeof body.num !== 'number' || body.num < 1 || body.num > 3) {
@@ -37,6 +43,7 @@ export class AiController {
       .then(res => res.map(answer => answer?.trimStart()))
   }
 
+  @Public()
   @Post('/weekly')
   async weekly(@Body() body: { text: string }) {
     return this.aiService.weekly(body.text).then(res => ({ answer: res?.trimStart() }))
