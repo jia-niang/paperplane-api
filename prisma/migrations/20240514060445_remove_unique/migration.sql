1c14b90d47f9bb725af13c31906d4e35e58fd8bf
jia-niang <1@paperplane.cc>
2024-05-14 14:05:50 +0800

    feat(schema): 移除所有 unique 的数据库字段

---
+++ prisma/migrations/20240514060445_remove_unique/migration.sql
@@ -0,0 +1,14 @@
+-- DropIndex
+DROP INDEX "GitProject_name_key";
+
+-- DropIndex
+DROP INDEX "GitRepo_name_key";
+
+-- DropIndex
+DROP INDEX "GitRepo_url_key";
+
+-- DropIndex
+DROP INDEX "GitStaff_name_key";
+
+-- DropIndex
+DROP INDEX "User_name_key";
