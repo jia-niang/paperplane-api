35452395e553200af0b93a130f8f8c840ee340a7
jia-niang <1@paperplane.cc>
2024-05-15 01:32:12 +0800

    feat(docker-status): 现在 docker status api 需要 Admin 权限

--- src/services/docker-status/docker-status.controller.ts
+++ src/services/docker-status/docker-status.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Get, Param } from '@nestjs/common'
 
+import { AdminRole } from '@/app/role.decorator'
+
 import { DockerStatusService } from './docker-status.service'
 
 @Controller('/docker-status')
@@ -7,12 +9,14 @@ export class DockerStatusController {
   constructor(private readonly dockerStatusService: DockerStatusService) {}
 
   /** 列出所有 Docker 容器 */
+  @AdminRole()
   @Get('/')
   async listAllContainers() {
     return this.dockerStatusService.listAll()
   }
 
   /** 按容器名获取状态 */
+  @AdminRole()
   @Get('/:containerName')
   async getContainerStatus(@Param('containerName') containerName: string) {
     return this.dockerStatusService.getStatus(containerName)
