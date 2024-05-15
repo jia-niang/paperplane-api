88e4042dee644f9398d49f8f30cfbc035cffea8e
jia-niang <1@paperplane.cc>
2024-05-16 02:09:09 +0800

    refactor(types): 现在部分全局类型改为导出类型 [ci skip]

--- src/services/third-party/third-party.service.ts
+++ src/services/third-party/third-party.service.ts
@@ -9,6 +9,28 @@ import { get, round, split } from 'lodash'
 
 import { setupRedisCache } from './setup-redis-cache'
 
+/** 下班提醒油价信息 */
+export interface IOffworkOilpriceInfo {
+  '92h': string
+  '95h': string
+  '98h': string
+  '0h': string
+}
+
+/** 下班提醒发薪日信息 */
+export interface IOffworkSalaryDayInfo {
+  salaryDate: string
+  salaryDateText: string
+  restDays: number
+}
+
+/** 下班提醒股价 */
+export interface IOffworkStockInfo {
+  today: number
+  yesterday: number
+  delta?: number
+}
+
 @Injectable()
 export class ThirdPartyService {
   juheClient: AxiosInstance
