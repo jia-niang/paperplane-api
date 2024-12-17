26f4f89c386bfda88ec3b20ae054a6713ca89b26
jia-niang <1@paperplane.cc>
2024-12-17 08:14:47 +0800

    fix(prisma): 提供 prisma 迁移

---
+++ prisma/migrations/20241217001431_set_default_for_new_fileds/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "OffworkViewRecord" ALTER COLUMN "imageUrl" SET DEFAULT '',
+ALTER COLUMN "shortUrl" SET DEFAULT '',
+ALTER COLUMN "trafficImageUrl" SET DEFAULT '',
+ALTER COLUMN "viewUrl" SET DEFAULT '';
